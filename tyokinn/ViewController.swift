2e4fce17d9c8c5a6fae41295f2531c26d2ae69a7
梶山清美 <ailovesingasong@kajiyamakiyoharunoMacBook-Air.local>
2020-12-20 12:22:43 +0900

    Initial Commit

---
+++ tyokinn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tyokinn
+//
+//  Created by 梶山清美 on 2020/12/20.
+//  Copyright © 2020年 梶山清美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
